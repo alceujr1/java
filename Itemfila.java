8c3c26b5f2782b4cd22a1183981d5c647aca1793
alceujr1 <144356500+alceujr1@users.noreply.github.com>
2023-09-22 00:43:50 -0300

    Add files via upload

---
+++ Itemfila.java
@@ -0,0 +1,31 @@
+package atividade;
+
+public class Itemfila {
+	private String nome;
+	private int codigo;
+	Itemfila proximo;
+
+	public Itemfila(String nome, int codigo) {
+		super();
+		this.nome = nome;
+		this.codigo = codigo;
+		this.proximo = null;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+}
