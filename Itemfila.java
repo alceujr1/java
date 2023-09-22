71b7c4bd4221e7471d4a3730561925b425ca8760
alceujr1 <144356500+alceujr1@users.noreply.github.com>
2023-09-22 00:45:57 -0300

    Update Itemfila.java

--- Itemfila.java
+++ Itemfila.java
@@ -1,5 +1,3 @@
-package atividade;
-
 public class Itemfila {
 	private String nome;
 	private int codigo;
@@ -29,3 +27,48 @@ public class Itemfila {
 	}
 
 }
+public class Fila {
+	Itemfila inicio;
+	Itemfila fim;
+
+	public Fila() {
+		super();
+		this.inicio = null;
+		this.fim = null;
+	}
+
+	public void botarFila(String novoNome, int novoCodigo) {
+		Itemfila novoItem = new Itemfila(novoNome, novoCodigo);
+		if (inicio == null) {
+			inicio = novoItem;
+			fim = novoItem;
+		} else {
+			fim.proximo = novoItem;
+			fim = novoItem;
+		}
+		System.out.println("Item adicionado à fila com sucesso.");
+
+	}
+	public void tirarFila() {
+        if (inicio == null) {
+            System.out.println("A fila está vazia.");
+        } else {
+            System.out.println("Item removido da fila: Nome: " + inicio.getNome() + ", Código: " + inicio.getCodigo());
+            inicio = inicio.proximo;
+        }
+    }
+	
+	public void exibirFila() {
+        if (inicio == null) {
+            System.out.println("A fila está vazia.");
+        } else {
+            System.out.println("Itens na fila:");
+            Itemfila atual = inicio;
+            while (atual != null) {
+                System.out.println("Nome: " + atual.getNome() + ", Código: " + atual.getCodigo());
+                atual = atual.proximo;
+            }
+        }
+    }
+}
+
